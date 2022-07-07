0fca40fced208958b0687f966f57dc82596bb461
Bik400 <biksingh30@gmail.com>
2022-07-07 13:42:30 -0700

    fixed bootstrap styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import FileUpload from "./components/FileUpload.js";
-import Dashboard from "./components/Dashboard.js";
+
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<FileUpload/>}></Route>
-        <Route path='/dashboard' element={<Dashboard/>}></Route>
-      </Routes>
-    </BrowserRouter>
+    <div className='container mt-4'>
+      <FileUpload />
+    </div>
   )
 }
 
